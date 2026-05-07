c6b1279c86e7967d166b06daa4863a69668a5944
regexghost <regexghost@protonmail.com>
2026-05-07 15:53:34 +0100

    added trash script

---
+++ terminal-scripts/rm-trash.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# wrapper script around the rm, and trash-cli commands
+
+command="trash-put"
+dialog="rm: Trash"
+
+if [ "$1" = "-p" ]; then
+	shift
+	command="/usr/bin/rm"
+	dialog="rm: Delete (Permanently)"
+else
+	if ! which trash-put > /dev/null; then
+		echo "trash-cli not installed"
+		exit 1
+	fi
+fi
+
+for arg; do
+	filename="$arg"
+
+	# Refuse to delete / or ~
+	if [ "$filename" = "$HOME" ] || [ "$filename" = "/" ]; then
+		echo "Refusing to delete"
+		exit
+	fi
+
+	! [ -e "$filename" ] && echo "File/Directory doesn't exit" && continue
+	if [ -d "$filename" ]; then
+		if [ -z "$(ls -"$filename")" ]; then
+			read -p "${dialog} ${filename} (empty dir) (y/N) " confirm
+		else
+			read -p "${dialog} ${filename} (non-empty dir) (y/N) " confirm
+		fi
+		[ "$uservar" = "y" ] && "$command" -- "$filename"
+		continue
+	fi
+
+	read -p "${dialog} ${filename} (y/N) " confirm
+	[ "$confirm" = "y" ] && "$command" -- "$filename"
+done
