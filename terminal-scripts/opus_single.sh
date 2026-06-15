4a0004f6b1e5881e695c0060da52a2da0c49dec8
regexghost <dev@regexghost.com>
2026-06-15 21:59:27 +0100

    added ny m4a -> opus conversion scripts

---
+++ terminal-scripts/opus_single.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+BITRATE_KBPS="100"
+
+oldIFS="$IFS"
+IFS=$'\n'
+files=( $(find m4asingle/ -type f | grep -v 'output.jpg$' | sort) )
+IFS="$oldIFS"
+
+[ -d opus/ ] && rm -rf opus
+mkdir opus
+
+i=0
+for file in "${files[@]}"; do
+	echo File: "$file"
+	outputFile="$(echo "$file" | sed 's/^m4asingle/opus/g' | sed 's/m4a$/opus/g')"
+	filename="$(basename "$file" | sed 's/.m4a//g')"
+	outputDir="$(dirname "$outputFile")"
+	mkdir -p "$outputDir"
+	
+	success=no
+	ffmpeg -y -i "$file" -map 0:1 "${outputDir}/${filename}.jpg" && success=yes
+	ffmpeg -i "$file" -b:a "${BITRATE_KBPS}000" "$outputFile"
+	[[ "$success" == "yes"  ]] && opustags --set-cover "${outputDir}/${filename}.jpg" "$outputFile" -i
+	i=$((i+1))
+done
---
+++ terminal-scripts/opus_soundtracks.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+BITRATE_KBPS="100"
+
+oldIFS="$IFS"
+IFS=$'\n'
+dirs=( $(find m4asoundtracks/ -type d -links 2 | sort) )
+soundtracks=( $(find m4asoundtracks/ -type f | grep -v 'output.jpg$' | sort) )
+IFS="$oldIFS"
+
+[ -d opus/ ] && rm -rf opus
+mkdir opus
+
+for dir in "${dirs[@]}"; do
+	echo "Dir: ${dir}"
+	firstFile="$(find "$dir" -type f | grep 'm4a$' |  head -n 1)"
+	outputDir="$(echo "$dir" | sed 's/^m4asoundtracks/opus/g')"
+	mkdir -p "$outputDir"
+	echo "First File: ${firstFile}"
+	ffmpeg -y -i "$firstFile" -map 0:1 "$dir/output.jpg"
+	cp "${dir}/output.jpg" "${outputDir}/output.jpg"
+	# [[ "$dir" == *"Other"* ]] && exit
+done
+
+i=0
+for file in "${soundtracks[@]}"; do
+	echo File: "$file"
+	outputFile="$(echo "$file" | sed 's/^m4asoundtracks/opus/g' | sed 's/m4a$/opus/g')"
+	outputDir="$(dirname "$outputFile")"
+	mkdir -p "$outputDir"
+	echo ffmpeg -i "$file" -b:a "${BITRATE_KBPS}000" "$outputFile"
+	ffmpeg -i "$file" -b:a "${BITRATE_KBPS}000" "$outputFile"
+	opustags --set-cover "$outputDir/output.jpg" "$outputFile" -i
+	i=$((i+1))
+done
