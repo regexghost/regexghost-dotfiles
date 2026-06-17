d604bea27788ef2b9ce80d619ce2b06c690e2ee8
regexghost <dev@regexghost.com>
2026-06-17 22:17:16 +0100

    added download-pods.sh and extension to queue-vids.sh (newsraft)

--- dotfiles/.config/newsraft/queue-vid.sh
+++ dotfiles/.config/newsraft/queue-vid.sh
@@ -11,8 +11,12 @@ if echo "$1" | grep -q "youtube.com/watch"; then
 elif echo "$1" | grep -q "youtube.com/shorts"; then
 	id="$(echo "$1" | sed -nE 's/.*\/(.*)/\1/p')"
 	setsid yt-data "$1" > "${LOC}/Shorts/${id}.txt" &
+elif echo "$1" | grep -q ".mp3"; then
+	echo "$1" >> "${HOME}/Videos/Podcasts/toDownload.txt"
+	notify-send "Podcast Queued"
+	exit
 else
-	notify-send "Not a video"
+	notify-send "Not a video or podcast"
 	exit
 fi
 
---
+++ terminal-scripts/download-pods.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+LOC="${HOME}/Videos/Podcasts"
+queueFile="${LOC}/toDownload.txt"
+
+cp "$queueFile" /tmp/toDownload.txt
+
+while read -r podcast_file; do
+	echo "$podcast_file"
+	curl -L "$podcast_file" > /tmp/out.mp3
+	if ! [ "$?" = "0" ]; then
+		mv /tmp/toDownload.txt "$queueFile"
+		echo "Error"
+		exit
+	fi
+	
+	filename="$(mediainfo /tmp/out.mp3  | grep -e "Track name" -e "Album" | sed 's/Track name/Trackname/g' | tr -s " " | cut -d " " -f 3- | tr  "\n" "+" | sed 's/[+]/ - /g' | sed 's/ - //g').mp3"
+	mv /tmp/out.mp3 "${LOC}/${filename}"
+	grep -v "$podcast_file" /tmp/toDownload.txt > /tmp/toDownload.txt.tmp
+	mv /tmp/toDownload.txt.tmp /tmp/toDownload.txt
+done < "$queueFile"
+
+mv /tmp/toDownload.txt "$queueFile"
