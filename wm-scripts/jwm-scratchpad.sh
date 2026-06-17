ddb20c3d14eda120c4fbfac7eb59ac2ed58c22f6
regexghost <dev@regexghost.com>
2026-06-17 20:33:39 +0100

    jwm-scratchpad (bound to Super+X)

---
+++ wm-scripts/jwm-scratchpad.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+scratchpad="$(xdotool search --class "scratchpad")"
+
+launch_window () {
+	tmux new-session -d -s "buffer_tmux" 'nano ~/Downloads/buffer.md; bash'
+	setsid /usr/local/bin/st -c "scratchpad" -t "buffer" -e tmux attach -t "buffer_tmux" &
+	sleep 0.5
+	scratchpad="$(xdotool search --class "scratchpad")"
+	xdotool windowmove "$scratchpad" 610 300
+}
+
+hide_window () {
+	xdotool windowmove "$scratchpad" -1000 -1000
+#	xdotool set_desktop_for_window "$scratchpad" 5
+	xdotool windowminimize "$scratchpad"
+}
+
+show_window () {
+	desktop="$(xdotool get_desktop)"
+	xdotool set_desktop_for_window "$scratchpad" "$desktop"
+	xdotool windowmove "$scratchpad" 610 300
+	xdotool windowactivate "$scratchpad"
+}
+
+if [ "$scratchpad" = "" ]; then
+	launch_window
+	exit
+fi
+
+focus="$(xdotool getwindowfocus)"
+if [ "$focus" = "$scratchpad" ]; then
+	hide_window
+else
+	show_window
+fi
+
