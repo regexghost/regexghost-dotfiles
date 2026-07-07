d424359a1fd5f112a05f544de52d87947ca318b9
regexghost <dev@regexghost.com>
2026-07-07 22:30:59 +0100

    added keepassxc.ini

--- apt-install.sh
+++ apt-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt install curl wget golang xorg xinit x11-xserver-utils bluetooth unzip git firefox mpv vlc tar moc fasd fonts-roboto fonts-firacode figlet jq xcompmgr picom trash-cli ncdu pulsemixer feh moc-ffmpeg-plugin keepassxc-full fzf libnotify-bin dunst texinfo xmlstarlet duf gh tidy w3m bluetooth
+sudo apt install curl wget golang xorg xinit x11-xserver-utils bluetooth unzip git firefox mpv vlc tar moc fasd fonts-roboto fonts-firacode figlet jq xcompmgr picom trash-cli ncdu pulsemixer feh moc-ffmpeg-plugin keepassxc-full fzf libnotify-bin dunst texinfo xmlstarlet duf gh tidy w3m bluetooth xclip
