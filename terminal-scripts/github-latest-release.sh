6582893924dd4f04e257bf1db3d41ce0a8c2eed9
regexghost <dev@regexghost.com>
2026-08-05 20:54:43 +0100

    redirector config descriptions update, new ublock origin filters, moved comment from codeberg to sourcehut in github-latest-relase.sh and fix for mymv.sh

--- terminal-scripts/github-latest-release.sh
+++ terminal-scripts/github-latest-release.sh
@@ -8,10 +8,10 @@ if [[ "$site" == "github" ]] || [[ "$site" == "gh" ]]; then
 	latest_tag=$(curl -w "%{redirect_url}" https://github.com/${user_name}/${repo_name}/releases/latest/ | sed 's/.*\///g')
 	echo https://github.com/${user_name}/${repo_name}/archive/refs/tags/${latest_tag}.zip
 elif [[ "$site" == "codeberg" ]] || [[ "$site" == "cb" ]]; then
-	# Sometimes this doesn't work due to browser checks
 	latest_tag=$(curl -w "%{redirect_url}" https://codeberg.org/${user_name}/${repo_name}/releases/latest/ | sed -nE 's/.*tag\/([^"]*)">.*/\1/p')
 	echo https://codeberg.org/${user_name}/${repo_name}/archive/${latest_tag}.zip
 elif [[ "$site" == "sourcehut" ]] || [[ "$site" == "srht" ]]; then
+	# Sometimes this doesn't work due to browser checks
 	latest_tag=$(curl "https://git.sr.ht/${user_name}/${repo_name}" | grep "/refs/" | cut -d "/" -f 5 | cut -d "\"" -f 1)
 	echo "https://git.sr.ht/${user_name}/${repo_name}/archive/${latest_tag}.zip"
 fi
--- terminal-scripts/mymv.sh
+++ terminal-scripts/mymv.sh
@@ -3,14 +3,16 @@
 # Wrapper for mv as suckless sbase doesn't have `mv -i`
 
 if [ "$#" -eq 2 ]; then
-	if [ -f "$2" ]; then
+	file="$2"
+	[ -d "$file" ] && file="${2}/$(basename "$1")"
+	if [ -f "$file" ]; then
 		read -p "Overwrite existing file? (y/N) " yesOrNo
 		if [ "$yesOrNo" = "y" ] || [ "$yesOrNo" = "Y" ]; then
-			mv "$1" "$2"
+			mv "$1" "$file"
 		fi
 		exit
 	else
-		mv "$1" "$2"
+		mv "$1" "$file"
 	fi
 else
 	mv "$@"
