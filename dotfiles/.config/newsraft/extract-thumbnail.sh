bf49606d2feb32cc23fba89c51e60b2af2130113
regexghost <dev@regexghost.com>
2026-08-07 20:27:17 +0100

    thumbnail extractor improvements, remove trailing dash after joining mocp output lines in lemonbar-bar

--- dotfiles/.config/newsraft/extract-thumbnail.sh
+++ dotfiles/.config/newsraft/extract-thumbnail.sh
@@ -6,7 +6,11 @@ OUTPUT_FILENAME="${2:-/tmp/youtube_thumbnail.webp}"
 video_id=$(echo $URL | sed 's/.*[?]v=//g')
 if echo "$video_id" | grep -q "www."; then
 	video_id=$(echo "$URL" | sed 's/.*\/shorts\///g')
+elif echo "$video_id" | grep -q "youtu.be"; then
+	video_id="$(echo "$URL" | sed 's/.*youtu.be\///g')"
 fi
-echo $video_id
+
+video_id="$(echo "$video_id" | sed 's/?.*//g')"
+
 wget "https://i.ytimg.com/vi_webp/${video_id}/maxresdefault.webp" -P /tmp
 mv /tmp/maxresdefault.webp "$OUTPUT_FILENAME"
